626f7fa89454147c8eb395ebff5de0fafbd7ba8f
YourGitHubUsername <your-email@example.com>
2026-04-24 19:09:45 +0500

    day6

--- Day 5/StackClass.py
+++ Day 5/StackClass.py
@@ -21,3 +21,50 @@ class Stack:
     
     def size(self):
         return len(self.stack)
+    
+class Queue:
+    
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if len(self.queue) == 0:
+            return "Queue is empty"
+        return self.queue.pop(0)
+    
+    def peek(self):
+        if len(self.queue) == 0:
+            return "Queue is empty"
+        return self.queue[0]
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+    def size(self):
+        return len(self.queue)
+    
+
+
+d = Stack()
+d.push(1)       
+d.push(2)
+d.push(3)
+print(d.is_empty())
+print(d.size())
+print(d.peek())
+print(d.pop())
+print(d.pop())
+print(d.is_empty())
+
+q = Queue()
+q.enqueue(1)            
+q.enqueue(2)
+q.enqueue(3)
+print(q.is_empty())
+print(q.size())
+print(q.peek())
+print(q.dequeue())
+print(q.dequeue())
+print(q.is_empty())
